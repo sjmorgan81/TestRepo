5000e3d8e12ea65166479135a782b1e242010228
Simon Morgan <simon.morgan@ceredigion.gov.uk>
2021-11-15 15:45:12 +0000

    Initial commit

---
+++ TestProject.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.Equals(2 + 2, 4);
+        }
+    }
+}
